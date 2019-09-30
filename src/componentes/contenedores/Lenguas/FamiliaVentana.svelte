6f4b269d07fa516c88c52d7a1914d50f8583ef00
josecaos <jazzvoon@gmail.com>
2019-09-30 18:42:18 -0500

    Avances Ventana Familias

--- src/componentes/contenedores/Lenguas/FamiliaVentana.svelte
+++ src/componentes/contenedores/Lenguas/FamiliaVentana.svelte
@@ -10,7 +10,7 @@
 
   export let familia;
   export let cerrar;
-  let cierraIcono = faTimesCircle
+  let cierraIcono = faTimesCircle;
 
   let elemento = { activo: false };
   const Mostrar = () => {
@@ -44,45 +44,6 @@
     max-height: 420px;
   }
 
-  .Familia {
-    display: flex;
-    position: absolute;
-    bottom: 4rem;
-    left: 4rem;
-    /* max-width: 20rem; */
-    /* max-height: 22rem; */
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    overflow-y: auto;
-    z-index: 10000;
-    background: #f7f7f7;
-    border-radius: 10px;
-  }
-
-  .Contenedor-1 {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    /* flex-wrap: nowrap; */
-    /* justify-content: space-between; */
-    overflow-y: auto;
-  }
-
-  i {
-    color: #bababa;
-  }
-
-  img {
-    width: 580px;
-    height: 455px;
-    object-fit: cover;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
-  }
-
   .Titulo {
     text-align: center;
     font-weight: 200;
@@ -102,16 +63,6 @@
     color: #e6aa30;
     margin: 0.5rem;
   }
-
-  .Saber {
-    width: 17%;
-    height: 10%;
-    border: 1px solid #e6aa30;
-    box-sizing: border-box;
-    border-radius: 15px;
-    color: #e6aa30;
-  }
-
   .TitulosLista {
     display: flex;
     justify-content: space-between;
@@ -119,21 +70,9 @@
     margin-left: -6rem;
   }
 
-  .TextoFotos {
-    position: relative;
-    top: -16%;
-    left: -39%;
-    color: #fff;
-  }
-
   .Flecha {
     width: 50%;
     margin-left: 10rem;
-  }
-
-  .Carrousel {
-    margin-top: -4.5rem;
-    font-size: 0.5rem;
   }
 
   .Contenedor-2 {
@@ -147,62 +86,128 @@
     top: 0.6%;
   }
 
-  .Lista {
-    margin-right: -5rem;
-    margin-left: -9.5rem;
-  }
-
   #Point {
     padding: 0 0.5rem 0 0;
   }
-  .FamiliaWrapper {
+  /*  */
+  /*  */
+  /* new */
+
+  .Familia {
+    /* display: flex; */
+    position: absolute;
+    bottom: 4rem;
+    left: 4rem;
+    /* justify-content: center; */
+    /* align-items: center; */
+    /* flex-direction: column; */
+    /* flex-wrap: nowrap; */
+    /* justify-content: space-between; */
+    overflow-y: auto;
+    z-index: 10000;
+    background: #f7f7f7;
+    border-radius: 10px;
+  }
+  .VentanaFamiliaWrapper {
     height: 100%;
     width: 100%;
   }
-  /* new */
-  .BotonWrapper {
+  .BotonCierraWrapper {
     display: flex;
     justify-content: right;
     height: 50px;
     width: 100%;
   }
-  .BotonIcono {
-    color: #878787;
-    font-size: 1rem;
+  .Encabezado {
+    height: auto;
+    width: 100%;
+  }
+  .BotonMasWrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 100%;
+  }
+  .Saber {
+    border: 1px solid #e6aa30;
+    box-sizing: border-box;
+    border-radius: 15px;
+    color: #e6aa30;
+    height: auto;
+    width: 35%;
+  }
+  .ContenedorImagenes {
+    display: flex;
+    position: relative;
+    align-items: center;
+    padding: 0.5rem;
+    flex-direction: column;
+  }
+  .ContenedorImagenes img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+  }
+  .TextoFotos {
+    position: absolute;
+    bottom: 1rem;
+    color: #fff;
+    left: 1rem;
+    height: auto;
+    width: 90%;
+  }
+  .TextoFotosSubTitulo {
+    font-size: 12px;
+  }
+  .Carrousel {
+    padding: 8px 0;
+    font-size: 0.5rem;
+  }
+  .Lista {
+    height: auto;
+    width: 100%;
   }
 </style>
 
 <article class="Familia">
 
-  <div class="FamiliaWrapper">
-    <div class="BotonWrapper">
+  <div class="VentanaFamiliaWrapper">
+
+    <div class="BotonCierraWrapper">
       <button class="BotonConIcono" on:click={cerrarVentana}>
         <Icon class="BotonIcono" icon={cierraIcono} />
       </button>
     </div>
-
+    <!--  -->
     <header class="Encabezado">
       <p class="Titulo">FAMILIA</p>
       <h1 class="Principal">Yuto-nahua</h1>
     </header>
-    {#if elemento.activo}
-      <button class="Saber" on:click={Mostrar}>Saber más</button>
-      <div class="FamiliaDetalle">
-        <FamiliaDetalle />
-      </div>
-    {/if}
+    <!--  -->
+    <div class="BotonMasWrapper">
+      {#if elemento.activo}
+        <button class="Saber" on:click={Mostrar}>Saber más</button>
+        <div class="FamiliaDetalle">
+          <FamiliaDetalle />
+        </div>
+      {/if}
 
-    {#if !elemento.activo}
-      <button class="Saber" on:click={Mostrar}>Saber más</button>
-    {/if}
-    <div class="Contenedor-1">
+      {#if !elemento.activo}
+        <button class="Saber" on:click={Mostrar}>Saber más</button>
+      {/if}
+    </div>
+    <!--  -->
+    <div class="ContenedorImagenes">
       <img
         class="Imagen"
         src={`http://unsplash.it/300/${100 + Math.floor(Math.random() * 500)}`}
         alt="img" />
       <div class="TextoFotos">
-        <p>Titulo Foto</p>
-        <p>Descripción</p>
+        <h4 class="TextoFotosTitulo">Titulo Foto</h4>
+        <p class="TextoFotosSubTitulo">Descripción</p>
       </div>
 
       <div class="Carrousel">
@@ -216,24 +221,23 @@
         <i id="Point" class="fas fa-circle fa-xs" />
         <i id="Point" class="fas fa-circle fa-xs" />
       </div>
-
-      <article class="Contenedor-2">
-        <span class="Flecha">
-          <i class="fas fa-chevron-down fa-3x" />
-        </span>
-        <header class="TitulosLista">
-          <h4>Agrupaciones Lingüísticas</h4>
-          <h4>Riesgo de desaparición</h4>
-        </header>
-        <section class="Lista">
-          <ul>
-            <Agrupacion />
-            <Agrupacion />
-            <Agrupacion />
-          </ul>
-        </section>
-      </article>
     </div>
 
+    <div class="Contenedor-2">
+      <span class="Flecha">
+        <i class="fas fa-chevron-down fa-3x" />
+      </span>
+      <header class="TitulosLista">
+        <h4>Agrupaciones Lingüísticas</h4>
+        <h4>Riesgo de desaparición</h4>
+      </header>
+      <section class="Lista">
+        <ul>
+          <Agrupacion />
+          <Agrupacion />
+          <Agrupacion />
+        </ul>
+      </section>
+    </div>
   </div>
 </article>
