40dac99c841186810bc8da70acf6d62e01bc3f47
josecaos <jazzvoon@gmail.com>
2019-09-30 22:37:20 -0500

    Ajustes ventana detalles familia

--- src/componentes/contenedores/Lenguas/FamiliaDetalle.svelte
+++ src/componentes/contenedores/Lenguas/FamiliaDetalle.svelte
@@ -1,5 +1,8 @@
 <script>
+  import Fa from "svelte-fa";
+  import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
+  let cierraIcono = faArrowCircleLeft;
 </script>
 
 <style>
@@ -22,7 +25,7 @@
     height: 390px;
   }
   .Texto p {
-      padding: 0 0.5rem;
+    padding: 0 0.5rem;
   }
   .OcultarWrapper {
     display: flex;
@@ -33,52 +36,52 @@
   .Ocultar {
     border: 0;
     background-color: #fff;
-    padding: 1rem 0 1rem 1rem;
   }
 </style>
 
 <section class="Detalle">
   <div class="OcultarWrapper">
-    <button class="Ocultar">
-      <i class="far fa-arrow-alt-circle-left fa-2x" />
+    <button class="BotonConIcono Ocultar">
+      <Fa icon={cierraIcono} />
     </button>
   </div>
   <section class="TextoWrapper">
     <div class="Texto">
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quod
-      quidem delectus hic aspernatur sit eum rem laborum sequi, recusandae
-      similique ut totam, dolores dignissimos veniam. Voluptatem error ipsam
-      ipsa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-      veritatis hic illo dolore a voluptate perspiciatis, beatae nisi odit
-      distinctio maiores reiciendis corporis obcaecati veniam, illum possimus
-      at? Delectus, vitae.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime
-      quisquam optio quis aut nihil vero, ex fugiat non totam quidem modi,
-      suscipit laboriosam molestiae placeat eligendi vel qui perspiciatis! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime quisquam
-      optio quis aut nihil vero, ex fugiat non totam quidem modi, suscipit
-      laboriosam molestiae placeat eligendi vel qui perspiciatis!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequuntur
-      autem repudiandae ducimus nobis possimus facere sunt sint vero laudantium
-      ex aperiam ipsa a, beatae neque, nostrum dolor animi sit! Lorem ipsum
-      dolor sit amet consectetur adipisicing elit. Quo consequuntur autem
-      repudiandae ducimus nobis possimus facere sunt sint vero laudantium ex
-      aperiam ipsa a, beatae neque, nostrum dolor animi sit!
-    </p>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
-      consequuntur commodi sed amet cum quasi minima voluptatum placeat est
-      necessitatibus. Atque repudiandae laboriosam illum ea, magnam voluptatum
-      sapiente architecto amet! Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit. Eius consequuntur commodi sed amet cum quasi minima
-      voluptatum placeat est necessitatibus. Atque repudiandae laboriosam illum
-      ea, magnam voluptatum sapiente architecto amet!
-    </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quod
+        quidem delectus hic aspernatur sit eum rem laborum sequi, recusandae
+        similique ut totam, dolores dignissimos veniam. Voluptatem error ipsam
+        ipsa! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Assumenda veritatis hic illo dolore a voluptate perspiciatis, beatae
+        nisi odit distinctio maiores reiciendis corporis obcaecati veniam, illum
+        possimus at? Delectus, vitae.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime
+        quisquam optio quis aut nihil vero, ex fugiat non totam quidem modi,
+        suscipit laboriosam molestiae placeat eligendi vel qui perspiciatis!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime
+        quisquam optio quis aut nihil vero, ex fugiat non totam quidem modi,
+        suscipit laboriosam molestiae placeat eligendi vel qui perspiciatis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+        consequuntur autem repudiandae ducimus nobis possimus facere sunt sint
+        vero laudantium ex aperiam ipsa a, beatae neque, nostrum dolor animi
+        sit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+        consequuntur autem repudiandae ducimus nobis possimus facere sunt sint
+        vero laudantium ex aperiam ipsa a, beatae neque, nostrum dolor animi
+        sit!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+        consequuntur commodi sed amet cum quasi minima voluptatum placeat est
+        necessitatibus. Atque repudiandae laboriosam illum ea, magnam voluptatum
+        sapiente architecto amet! Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Eius consequuntur commodi sed amet cum quasi minima
+        voluptatum placeat est necessitatibus. Atque repudiandae laboriosam
+        illum ea, magnam voluptatum sapiente architecto amet!
+      </p>
     </div>
   </section>
 </section>
